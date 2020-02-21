f02a69a7d64f07f6a3c5b4c74dd0714c07d10a13
KURTGODEL <erickcpgomes@gmail.com>
2020-02-21 09:03:47 -0500

    Initial Commit

---
+++ TableViewExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewExample
+//
+//  Created by Student on 2020-02-21.
+//  Copyright © 2020 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
